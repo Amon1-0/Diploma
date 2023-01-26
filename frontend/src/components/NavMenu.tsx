cd3db96e1957188c4aea1823486e9c48e3c6045a
Amon1-0 <amonmadzidov61@gmail.com>
2023-01-26 19:02:33 +0200

    Navigation menu is implemented

---
+++ frontend/src/components/NavMenu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import logo from '../assets/images/logo.jpg';
+const NavMenu = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src={logo} alt="Logo"/>
+            </div>
+            <ul>
+                <li><Link to={'/home'}>Home</Link></li>
+                <li><Link to={'/profile'}>Profile</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavMenu;
---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavMenu from "../components/NavMenu";
+import MainContent from "../components/MainContent";
+
+const Main = () => {
+    return (
+        <div>
+            <NavMenu/>
+            <MainContent/>
+        </div>
+    );
+};
+
+export default Main;
